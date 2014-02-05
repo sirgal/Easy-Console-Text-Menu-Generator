06a19f2707a5a88a0d1ec9bd7803705e230b2d96
sirgal <sirgal@users.noreply.github.com>
2014-02-05 21:52:08 +0200

    Create handler_functions.h

---
+++ example/handler_functions.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void function_1( ) { printf( "Function 1 called\n" ); }
+void function_2( ) { printf( "Function 2 called\n" ); }
+void function_3( ) { printf( "Function 3 called\n" ); }
+void function_4( ) { printf( "Function 4 called\n" ); }
+void function_5( ) { printf( "Function 5 called\n" ); }
